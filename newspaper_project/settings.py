47b73fab36fcf00233262f5e4c402074515dbe43
brendan <brendangaturu@gmail.com>
2019-10-03 22:53:04 +0300

    articles app with edit, update and delete functionality

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
+    'articles.apps.ArticlesConfig',
     # 3rd part apps
     'crispy_forms',
     'django.contrib.admin',
