784908e1813d16f8a2652dfdfd14dcd465d93729
brendan <brendangaturu@gmail.com>
2019-10-07 09:43:13 +0300

    comment functionality

--- articles/admin.py
+++ articles/admin.py
@@ -3,5 +3,16 @@ from .models import Article, Comment
 
 # Register your models here.
 
-admin.site.register(Article)
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Comment)
--- articles/models.py
+++ articles/models.py
@@ -17,7 +17,9 @@ class Article(models.Model):
 
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE,)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE,
+                                # setting the name of the reverse relationship from FOO_set
+                                related_name='comments',)
     comment = models.CharField(max_length=200)
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,)
 
